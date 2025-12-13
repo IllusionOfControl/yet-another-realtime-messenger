f66fa10b2e42b4a0618c3961f5eb83a4974f3a95
Sergey Skorokhod <sergeyskorokhod2@gmail.com>
2025-12-13 23:17:19 +0100

    [user-service] fix models. update docker file

--- user-service/app/models.py
+++ user-service/app/models.py
@@ -12,10 +12,10 @@ from sqlalchemy import (
     func,
 )
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from app.database import Base
-
+from datetime import datetime
 
 class ContactStatus(enum.Enum):
     FRIEND = "FRIEND"
@@ -25,16 +25,16 @@ class ContactStatus(enum.Enum):
 class UserProfile(Base):
     __tablename__ = "user_profiles"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    username = Column(String, unique=True, index=True, nullable=False)
-    display_name = Column(String, nullable=True)
-    email = Column(String, unique=True, index=True, nullable=True)
-    bio = Column(String, nullable=True)
-    avatar_file_id = Column(UUID(as_uuid=True), nullable=True)
-    custom_status = Column(String, nullable=True)
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
-    is_active = Column(Boolean, default=True, nullable=False)
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    username: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
+    display_name: Mapped[str] = mapped_column(nullable=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True, nullable=True)
+    bio: Mapped[str] = mapped_column(nullable=True)
+    avatar_file_id: Mapped[str] = mapped_column(nullable=True)
+    custom_status: Mapped[str] = mapped_column(nullable=True)
+    created_at: Mapped[datetime] = mapped_column(default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(default=func.now(), onupdate=func.now())
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
 
     owned_contacts = relationship(
         "UserContact",
@@ -58,15 +58,11 @@ class UserProfile(Base):
 class UserContact(Base):
     __tablename__ = "user_contacts"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    owner_id = Column(
-        UUID(as_uuid=True), ForeignKey("user_profiles.id"), nullable=False
-    )
-    contact_user_id = Column(
-        UUID(as_uuid=True), ForeignKey("user_profiles.id"), nullable=False
-    )
-    status = Column(Enum(ContactStatus), nullable=False)
-    created_at = Column(DateTime, default=func.now())
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    owner_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user_profiles.id"), nullable=False)
+    contact_user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user_profiles.id"), nullable=False)
+    status: Mapped[ContactStatus] = mapped_column(Enum(ContactStatus), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
 
     __table_args__ = (
         UniqueConstraint("owner_id", "contact_user_id", name="_owner_contact_uc"),
--- user-service/app/settings.py
+++ user-service/app/settings.py
@@ -15,34 +15,13 @@ class Settings(BaseSettings):
     app_port: int = 8000
 
     database_url: str
+
+    service_token: str
     file_upload_service_url: str
     file_upload_service_token: str
 
     log_level: str = Field("info")
     log_format: str = Field("text")
-
-
-class ProdSettings(Settings):
-    model_config = SettingsConfigDict(env_file=".env.prod", extra="ignore")
-    ENV: str = "prod"
-
-
-class DevSettings(Settings):
-    model_config = SettingsConfigDict(env_file=".env.dev", extra="ignore")
-    ENV: str = "dev"
-
-
-class TestSettings(Settings):
-    model_config = SettingsConfigDict(env_file="f" "", extra="ignore")
-    ENV: str = "test"
-
-
-environments: dict[str, type[Settings]] = {
-    "prod": ProdSettings,
-    "dev": DevSettings,
-    "test": TestSettings,
-}
-
 
 @lru_cache()
 def get_settings() -> Settings:
